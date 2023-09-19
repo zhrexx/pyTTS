ffcf26c8c3f441142953fced8a55e15f9c28105d
zhrexx <134622710+zhrexx@users.noreply.github.com>
2023-09-19 13:11:01 +0200

    Update pyTTS.py

--- pyTTS.py
+++ pyTTS.py
@@ -1,7 +1,12 @@
+def load(model_file_name):
+  with open(model_file_name,"r") as md:
+    md.read()
+    
+      
 
-def load("model_file_name.ttsm3"):
-  pass
-
-def text("text"):
+def text(text):
+  with open("text.txt","w") as tx:
+    tx.write()
    pass
 
+def generate_speech(self):
