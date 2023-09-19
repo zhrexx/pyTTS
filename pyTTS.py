bf3b2eebf729299929b6daa975f177dd5b7acaca
zhrexx <134622710+zhrexx@users.noreply.github.com>
2023-09-19 13:45:14 +0200

    Update pyTTS.py

--- pyTTS.py
+++ pyTTS.py
@@ -1,12 +1,27 @@
+
+def __init__(self):
+  self.rate = 66000
+  self.duration = 0.1
+  self.stop = 3.00
+  self.generation = True
+
+
 def load(model_file_name):
-  with open(model_file_name,"r") as md:
+  with open(model_file_name,"rb") as md:
     md.read()
     
       
 
 def text(text):
-  with open("text.txt","w") as tx:
-    tx.write()
-   pass
+  with open("textogen.txt","w") as tx:
+    tx.write(text)
+  
+   
 
 def generate_speech(self):
+  pass
+
+
+def error(self):
+  self.generation = False
+  
