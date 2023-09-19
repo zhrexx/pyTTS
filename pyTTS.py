14c01b5fd7b691448a43f6e6ccc8a03d7a288b13
zhrexx <134622710+zhrexx@users.noreply.github.com>
2023-09-19 18:03:56 +0200

    Update pyTTS.py

--- pyTTS.py
+++ pyTTS.py
@@ -1,35 +1,25 @@
+import pyttsx3
 
-def __init__(self):
-  self.rate = 66000
-  self.duration = 0.1
-  self.stop = 3.00
-  self.generation = True
+class SpeechGenerator:
+    def __init__(self):
+        self.generation = True
+        self.engine = pyttsx3.init()
 
+    def text_to_speech(self, text):
+        if self.generation:
+            try:
+                self.engine.say(text)
+                self.engine.runAndWait()
+            except Exception as e:
+                print(f"Error generating speech: {e}")
+        else:
+            print("Error 909")
 
-def load(model_file_name):
-  try:
-      with open(model_file_name,"rb") as md:
-         md.read()
-  except:
-    self.generation = False
-    
-      
+# Example usage:
+generator = SpeechGenerator()
+generator.text_to_speech("Hello, this is a test.")
 
-def text(text):
-  if self.generation == True:
-      with open("textogen.txt","w") as tx:
-         tx.write(text)
-  else:
-    
   
-   
-
-def generate_speech(self):
-  if self.generation == True:
-    
-    else:
-      print("Error 909")
-  pass
 
 
 
